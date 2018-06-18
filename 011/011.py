2f40bd24a16457efcde1be23bbb779d88266a7fe
KevinPei <769356170@qq.com>
2018-06-18 09:52:29 +0800

    011

--- 011/011.py
+++ 011/011.py
@@ -1,7 +1,6 @@
 def get_input(input_string):
     with open('filtered_words.txt') as text:
         sensitive_words = text.readlines()
-        print(sensitive_words)
 
         for word in sensitive_words:
             word = word.replace('\n', '')
